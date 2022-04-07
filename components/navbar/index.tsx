331cd3112719f84fc1906b8723dc492653455b0c
Filip Jerga <jerga99@gmail.com>
2022-04-07 19:38:10 +0100

    cleanup

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -23,7 +23,7 @@ export default function Navbar() {
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
+                {/* Mobile menu butt*/}
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
