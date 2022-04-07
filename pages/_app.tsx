06765f5bd583074201805297a99a14e16427c81c
Filip Jerga <jerga99@gmail.com>
2022-04-07 10:23:54 +0100

    Navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
+
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Navbar } from '../components'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
