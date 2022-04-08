63effbca3eb5ea629d81b62793a736807b9d6e81
Filip Jerga <jerga99@gmail.com>
2022-04-08 14:43:54 +0100

    Profile Link

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
-import Link from 'next/link'
+import { Disclosure, Menu } from '@headlessui/react';
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
+import Link from 'next/link';
 
 const navigation = [
   { name: 'Marketplace', href: '/', current: true },
@@ -85,32 +84,13 @@ export default function Navbar() {
                   <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <Menu.Item>
                       {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                        >
-                          Your Profile
-                        </a>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                        >
-                          Settings
-                        </a>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                        >
-                          Sign out
-                        </a>
+                        <Link href="/profile">
+                          <a
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Your Profile
+                          </a>
+                        </Link>
                       )}
                     </Menu.Item>
                   </Menu.Items>
---
+++ pages/profile.tsx
@@ -0,0 +1,16 @@
+
+
+
+import { NextPage } from "next";
+import { BaseLayout } from "../components";
+
+const Profile: NextPage = () => {
+
+  return (
+    <BaseLayout>
+      Profile
+    </BaseLayout>
+  )
+}
+
+export default Profile;
