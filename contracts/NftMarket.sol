9761860dfca4a6eec88da0ff0f977a99a7087300
Filip Jerga <jerga99@gmail.com>
2022-04-12 09:06:54 +0100

    ERC721

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract NftMarket {}
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NftMarket is ERC721URIStorage {
+  constructor() ERC721("CreaturesNFT", "CNFT") {}
+}
