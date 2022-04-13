a935e5c171fa40e4c2bbf25bad55ee6d62cb2b77
Filip Jerga <jerga99@gmail.com>
2022-04-13 08:16:43 +0100

    Generic Types

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -3,7 +3,7 @@ import { CryptoHookFactory } from "@_types/hooks";
 import useSWR from "swr";
 
 // deps -> provider, ethereum, contract (web3State)
-export const hookFactory: CryptoHookFactory = (deps) => (params) => {
+export const hookFactory: CryptoHookFactory<string, string> = (deps) => (params) => {
   const swrRes = useSWR("web3/useAccount", () => {
     console.log(deps);
     console.log(params);
--- types/hooks.ts
+++ types/hooks.ts
@@ -9,10 +9,17 @@ export type Web3Dependencies = {
   ethereum: MetaMaskInpageProvider
 }
 
-export type CryptoSWRResponse = SWRResponse;
-
-export type CryptoHandlerHook = (params: string) => CryptoSWRResponse
-
-export type CryptoHookFactory = {
-  (d: Partial<Web3Dependencies>): CryptoHandlerHook
+export type CryptoHookFactory<D = any, P = any> = {
+  (d: Partial<Web3Dependencies>): CryptoHandlerHook<D, P>
 }
+
+export type CryptoHandlerHook<D = any, P = any> = (params: P) => CryptoSWRResponse<D>
+
+export type CryptoSWRResponse<D = any> = SWRResponse<D>;
+
+
+// export type CryptoHookFactory<D = any, P = any> = {
+//   (d: Partial<Web3Dependencies>): (params: P) => SWRResponse<D>
+// }
+
+
