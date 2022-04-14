6c73fec8819338384ab6d0fee76686bafa79fabe
Filip Jerga <jerga99@gmail.com>
2022-04-14 11:44:02 +0100

    Network Name

--- components/hooks/web3/useNetwork.ts
+++ components/hooks/web3/useNetwork.ts
@@ -2,6 +2,16 @@
 import { CryptoHookFactory } from "@_types/hooks";
 import useSWR from "swr";
 
+const NETWORKS: {[k: string]: string} = {
+  1: "Ethereum Main Network",
+  3: "Ropsten Test Network",
+  4: "Rinkeby Test Network",
+  5: "Goerli Test Network",
+  42: "Kovan Test Network",
+  56: "Binance Smart Chain",
+  1337: "Ganache",
+}
+
 type UseNetworkResponse = {
   isLoading: boolean;
 }
@@ -14,8 +24,13 @@ export const hookFactory: NetworkHookFactory = ({provider, isLoading}) => () => 
   const {data, isValidating, ...swr} = useSWR(
     provider ? "web3/useNetwork" : null,
     async () => {
+      const chainId = (await provider!.getNetwork()).chainId;
 
-      return "Testing Network";
+      if (!chainId) {
+        throw "Cannot retreive network. Please, refresh browser or connect to other one."
+      }
+
+      return NETWORKS[chainId];
     }, {
       revalidateOnFocus: false
     }
