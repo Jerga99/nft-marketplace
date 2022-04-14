1238614db5dca2fd9443cb7b6ca947a693b63de4
Filip Jerga <jerga99@gmail.com>
2022-04-14 11:24:42 +0100

    Wallet conditionals

--- components/ui/navbar/Walletbar.tsx
+++ components/ui/navbar/Walletbar.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 
-import { FunctionComponent } from "react"
+import { Menu } from "@headlessui/react";
+import Link from "next/link";
+import { FunctionComponent } from "react";
 
 type WalletbarProps = {
   isLoading: boolean;
@@ -8,6 +11,9 @@ type WalletbarProps = {
   connect: () => void;
 }
 
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
 
 const Walletbar: FunctionComponent<WalletbarProps> = ({
   isInstalled,
@@ -16,20 +22,90 @@ const Walletbar: FunctionComponent<WalletbarProps> = ({
   account
 }) => {
 
-  console.log("Is Loading: ", isLoading);
-  console.log("Is Installed: ", isInstalled);
+  if (isLoading) {
+    return (
+      <div>
+        <button
+          onClick={() => {}}
+          type="button"
+          className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Loading ...
+        </button>
+      </div>
+    )
+  }
 
-  return (
-    <button
-      onClick={() => {
-        connect();
-      }}
-      type="button"
-      className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-    >
-      Connect Wallet
-    </button>
-  )
+  if (account) {
+    return (
+      <Menu as="div" className="ml-3 relative">
+        <div>
+          <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+            <span className="sr-only">Open user menu</span>
+            <img
+              className="h-8 w-8 rounded-full"
+              src="/images/default_user_image.png"
+              alt=""
+            />
+          </Menu.Button>
+        </div>
+
+        <Menu.Items className="z-10 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Item>
+            {() => (
+              <button
+                disabled={true}
+                className="disabled:text-gray-500 text-xs block px-4 pt-2 text-gray-700">
+                {`0x${account[2]}${account[3]}${account[4]}....${account.slice(-4)}`}
+              </button>
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <Link href="/profile">
+                <a
+                  className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                >
+                  Profile
+                </a>
+              </Link>
+
+            )}
+          </Menu.Item>
+        </Menu.Items>
+      </Menu>
+    )
+  }
+
+  if (isInstalled) {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            connect()
+          }}
+          type="button"
+          className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Connect Wallet
+        </button>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            window.open ('https://metamask.io', '_ blank');
+          }}
+          type="button"
+          className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          No Wallet
+        </button>
+      </div>
+    )
+  }
 }
 
 export default Walletbar;
