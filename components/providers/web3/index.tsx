a8d4e925607c891870ca7284744a07ecfe745331
Filip Jerga <jerga99@gmail.com>
2022-04-14 09:49:41 +0100

    Handle error when no wallet

--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -9,15 +9,23 @@ const Web3Provider: FunctionComponent = ({children}) => {
 
   useEffect(() => {
     async function initWeb3() {
-      const provider = new ethers.providers.Web3Provider(window.ethereum as any);
-      const contract =  await loadContract("NftMarket", provider);
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum as any);
+        const contract =  await loadContract("NftMarket", provider);
 
-      setWeb3Api(createWeb3State({
-        ethereum: window.ethereum,
-        provider,
-        contract,
-        isLoading: false
-      }))
+        setWeb3Api(createWeb3State({
+          ethereum: window.ethereum,
+          provider,
+          contract,
+          isLoading: false
+        }))
+      } catch(e: any) {
+        console.error("Please, install web3 wallet");
+        setWeb3Api((api) => createWeb3State({
+          ...api as any,
+          isLoading: false,
+        }))
+      }
     }
 
     initWeb3();
