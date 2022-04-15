017bc6085390956912b098a12e1f7cf59f9b666e
Filip Jerga <jerga99@gmail.com>
2022-04-15 12:59:05 +0100

    Target network

--- components/hooks/web3/useNetwork.ts
+++ components/hooks/web3/useNetwork.ts
@@ -12,8 +12,13 @@ const NETWORKS: {[k: string]: string} = {
   1337: "Ganache",
 }
 
+const targetId = process.env.NEXT_PUBLIC_TARGET_CHAIN_ID as string;
+const targetNetwork = NETWORKS[targetId];
+
 type UseNetworkResponse = {
   isLoading: boolean;
+  isSupported: boolean;
+  targetNetwork: string;
 }
 
 type NetworkHookFactory = CryptoHookFactory<string, UseNetworkResponse>
@@ -40,6 +45,8 @@ export const hookFactory: NetworkHookFactory = ({provider, isLoading}) => () => 
     ...swr,
     data,
     isValidating,
+    targetNetwork,
+    isSupported: data === targetNetwork,
     isLoading: isLoading || isValidating,
   };
 }
--- components/ui/navbar/index.tsx
+++ components/ui/navbar/index.tsx
@@ -69,7 +69,9 @@ export default function Navbar() {
                     <svg className="-ml-0.5 mr-1.5 h-2 w-2 text-indigo-400" fill="currentColor" viewBox="0 0 8 8">
                       <circle cx={4} cy={4} r={3} />
                     </svg>
-                    {network.data}
+                    {network.data} ---
+                    {`Is supported: ${network.isSupported}`} ---
+                    Target: {network.targetNetwork}
                   </span>
                 </div>
                 <Walletbar
