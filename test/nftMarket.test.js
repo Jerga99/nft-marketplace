f92094016883e719b921e30f4b74bcfc5ad2feca
Filip Jerga <jerga99@gmail.com>
2022-04-16 19:10:24 +0100

    Mint token test

--- test/nftMarket.test.js
+++ test/nftMarket.test.js
@@ -6,14 +6,19 @@ contract("NftMarket", accounts => {
 
   before(async () => {
     _contract = await NftMarket.deployed();
-    console.log(accounts);
   })
 
   describe("Mint token", () => {
+    const tokenURI = "https://test.com";
+    before(async () => {
+      await _contract.mintToken(tokenURI, {
+        from: accounts[0]
+      })
+    })
 
-    it("should resolve into true value", () => {
-      let numberOfNfts = 12;
-      assert(numberOfNfts == 12, "Value is NOT true");
+    it("owner of the first token should be address[0]", async () => {
+      const owner = await _contract.ownerOf(1);
+      assert.equal(owner, accounts[0], "Owner of token is not matching address[0]");
     })
   })
 
