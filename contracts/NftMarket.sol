b2508ab9ef495b9ae7f7886a6e8276e348f3c3b2
Filip Jerga <jerga99@gmail.com>
2022-04-16 18:35:35 +0100

    Mint Token

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -2,7 +2,27 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NftMarket is ERC721URIStorage {
+  using Counters for Counters.Counter;
+
+  Counters.Counter private _listedItems;
+  Counters.Counter private _tokenIds;
+
+
   constructor() ERC721("CreaturesNFT", "CNFT") {}
+
+  function mintToken(string memory tokenURI) public payable returns (uint) {
+    _tokenIds.increment();
+    _listedItems.increment();
+
+    uint newTokenId = _tokenIds.current();
+
+    _safeMint(msg.sender, newTokenId);
+    _setTokenURI(newTokenId, tokenURI);
+
+    return newTokenId;
+  }
+
 }
