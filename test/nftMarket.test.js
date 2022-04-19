28c81d53739b2a87611b8a855f84fe9e5a41952b
Filip Jerga <jerga99@gmail.com>
2022-04-19 12:46:07 +0100

    Buy NFT test

--- test/nftMarket.test.js
+++ test/nftMarket.test.js
@@ -55,4 +55,28 @@ contract("NftMarket", accounts => {
       assert.equal(nftItem.isListed, true, "Token is not listed");
     })
   })
+
+  describe("Buy NFT", () => {
+    before(async () => {
+      await _contract.buyNft(1, {
+        from: accounts[1],
+        value: _nftPrice
+      })
+    })
+
+    it("should unlist the item", async () => {
+      const listedItem = await _contract.getNftItem(1);
+      assert.equal(listedItem.isListed, false, "Item is still listed");
+    })
+
+    it("should decrease listed items count", async () => {
+      const listedItemsCount = await _contract.listedItemsCount();
+      assert.equal(listedItemsCount.toNumber(), 0, "Count has not been decrement");
+    })
+
+    it("should change the owner", async () => {
+      const currentOwner = await _contract.ownerOf(1);
+      assert.equal(currentOwner, accounts[1], "Item is still listed");
+    })
+  })
 })
