3fb29582b7a644102970d847f31fe67c0f0910a4
Filip Jerga <jerga99@gmail.com>
2022-04-19 08:17:41 +0100

    Check if token uri exists

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -10,10 +10,14 @@ contract NftMarket is ERC721URIStorage {
   Counters.Counter private _listedItems;
   Counters.Counter private _tokenIds;
 
+  mapping(string => bool) private _usedTokenURIs;
+
 
   constructor() ERC721("CreaturesNFT", "CNFT") {}
 
   function mintToken(string memory tokenURI) public payable returns (uint) {
+    require(!tokenURIExists(tokenURI), "Token URI already exists");
+
     _tokenIds.increment();
     _listedItems.increment();
 
@@ -21,8 +25,13 @@ contract NftMarket is ERC721URIStorage {
 
     _safeMint(msg.sender, newTokenId);
     _setTokenURI(newTokenId, tokenURI);
+    _usedTokenURIs[tokenURI] = true;
 
     return newTokenId;
+  }
+
+  function tokenURIExists(string memory tokenURI) public view returns (bool) {
+    return _usedTokenURIs[tokenURI] == true;
   }
 
 }
