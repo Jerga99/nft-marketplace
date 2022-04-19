67758148f569300278cf48e1ef52fcefc0b2e9e7
Filip Jerga <jerga99@gmail.com>
2022-04-19 08:56:24 +0100

    Create nft Item

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -11,11 +11,25 @@ contract NftMarket is ERC721URIStorage {
   Counters.Counter private _tokenIds;
 
   mapping(string => bool) private _usedTokenURIs;
+  mapping(uint => NftItem) private _idToNftItem;
 
+  struct NftItem {
+    uint tokenId;
+    uint price;
+    address creator;
+    bool isListed;
+  }
+
+  event NftItemCreated (
+    uint tokenId,
+    uint price,
+    address creator,
+    bool isListed
+  );
 
   constructor() ERC721("CreaturesNFT", "CNFT") {}
 
-  function mintToken(string memory tokenURI) public payable returns (uint) {
+  function mintToken(string memory tokenURI, uint price) public payable returns (uint) {
     require(!tokenURIExists(tokenURI), "Token URI already exists");
 
     _tokenIds.increment();
@@ -25,9 +39,26 @@ contract NftMarket is ERC721URIStorage {
 
     _safeMint(msg.sender, newTokenId);
     _setTokenURI(newTokenId, tokenURI);
+    _createNftItem(newTokenId, price);
     _usedTokenURIs[tokenURI] = true;
 
     return newTokenId;
+  }
+
+  function _createNftItem(
+    uint tokenId,
+    uint price
+  ) private {
+    require(price > 0, "Price must be at least 1 wei");
+
+    _idToNftItem[tokenId] = NftItem(
+      tokenId,
+      price,
+      msg.sender,
+      true
+    );
+
+    emit NftItemCreated(tokenId, price, msg.sender, true);
   }
 
   function tokenURIExists(string memory tokenURI) public view returns (bool) {
