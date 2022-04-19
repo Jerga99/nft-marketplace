aade3f3a20f5f4a981262ee0cbd111c9e94ae87c
Filip Jerga <jerga99@gmail.com>
2022-04-19 12:36:10 +0100

    Buy NFT

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -60,6 +60,22 @@ contract NftMarket is ERC721URIStorage {
     return newTokenId;
   }
 
+  function buyNft(
+    uint tokenId
+  ) public payable {
+    uint price = _idToNftItem[tokenId].price;
+    address owner = ERC721.ownerOf(tokenId);
+
+    require(msg.sender != owner, "You already own this NFT");
+    require(msg.value == price, "Please submit the asking price");
+
+    _idToNftItem[tokenId].isListed = false;
+    _listedItems.decrement();
+
+    _transfer(owner, msg.sender, tokenId);
+    payable(owner).transfer(msg.value);
+  }
+
   function _createNftItem(
     uint tokenId,
     uint price
