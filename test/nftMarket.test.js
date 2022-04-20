da71e9f4ee6cfdc8a9e7b724abed7a14dc9e5b3c
Filip Jerga <jerga99@gmail.com>
2022-04-20 09:33:11 +0100

    Test token transfers

--- test/nftMarket.test.js
+++ test/nftMarket.test.js
@@ -79,4 +79,28 @@ contract("NftMarket", accounts => {
       assert.equal(currentOwner, accounts[1], "Item is still listed");
     })
   })
+
+  describe("Token transfers", () => {
+    const tokenURI = "https://test-json-2.com";
+    before(async () => {
+      await _contract.mintToken(tokenURI, _nftPrice, {
+        from: accounts[0],
+        value: _listingPrice
+      })
+    })
+
+    it("should have two NFTs created", async () => {
+      const totalSupply = await _contract.totalSupply();
+      assert.equal(totalSupply.toNumber(), 2, "Total supply of token is not correct");
+    })
+
+    it("should be able to retreive nft by index", async () => {
+      const nftId1 = await _contract.tokenByIndex(0);
+      const nftId2 = await _contract.tokenByIndex(1);
+
+
+      assert.equal(nftId1.toNumber(), 1, "Nft id is wrong");
+      assert.equal(nftId2.toNumber(), 2, "Nft id is wrong");
+    })
+  })
 })
