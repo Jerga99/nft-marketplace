342580655e32a2f89fb0ebd7f50c5ae21160da0b
Filip Jerga <jerga99@gmail.com>
2022-04-20 09:07:54 +0100

    Add token to all enumaration

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -19,8 +19,12 @@ contract NftMarket is ERC721URIStorage {
   Counters.Counter private _listedItems;
   Counters.Counter private _tokenIds;
 
+  uint256[] private _allNfts;
+
   mapping(string => bool) private _usedTokenURIs;
   mapping(uint => NftItem) private _idToNftItem;
+
+  mapping(uint => uint) private _idToNftIndex;
 
   event NftItemCreated (
     uint tokenId,
@@ -41,6 +45,15 @@ contract NftMarket is ERC721URIStorage {
 
   function tokenURIExists(string memory tokenURI) public view returns (bool) {
     return _usedTokenURIs[tokenURI] == true;
+  }
+
+  function totalSupply() public view returns (uint) {
+    return _allNfts.length;
+  }
+
+  function tokenByIndex(uint index) public view returns (uint) {
+    require(index < totalSupply(), "Index out of bounds");
+    return _allNfts[index];
   }
 
   function mintToken(string memory tokenURI, uint price) public payable returns (uint) {
@@ -91,4 +104,23 @@ contract NftMarket is ERC721URIStorage {
 
     emit NftItemCreated(tokenId, price, msg.sender, true);
   }
+
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint tokenId
+  ) internal virtual override {
+    super._beforeTokenTransfer(from, to, tokenId);
+
+    // minting token
+    if (from == address(0)) {
+      _addTokenToAllTokensEnumaration(tokenId);
+    }
+  }
+
+  function _addTokenToAllTokensEnumaration(uint tokenId) private {
+    _idToNftIndex[tokenId] = _allNfts.length;
+    _allNfts.push(tokenId);
+  }
+
 }
