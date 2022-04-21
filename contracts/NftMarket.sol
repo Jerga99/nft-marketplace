2de767afe4d0e4e473739d387ba313e39ecd4501
Filip Jerga <jerga99@gmail.com>
2022-04-21 08:15:20 +0100

    Get owned Nfts

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -58,6 +58,11 @@ contract NftMarket is ERC721URIStorage {
     return _allNfts[index];
   }
 
+  function tokenOfOwnerByIndex(address owner, uint index) public view returns (uint) {
+    require(index < ERC721.balanceOf(owner), "Index out of bounds");
+    return _ownedTokens[owner][index];
+  }
+
   function getAllNftsOnSale() public view returns (NftItem[] memory) {
     uint allItemsCounts = totalSupply();
     uint currentIndex = 0;
@@ -71,6 +76,19 @@ contract NftMarket is ERC721URIStorage {
         items[currentIndex] = item;
         currentIndex += 1;
       }
+    }
+
+    return items;
+  }
+
+  function getOwnedNfts() public view returns (NftItem[] memory) {
+    uint ownedItemsCount = ERC721.balanceOf(msg.sender);
+    NftItem[] memory items = new NftItem[](ownedItemsCount);
+
+    for (uint i = 0; i < ownedItemsCount; i++) {
+      uint tokenId = tokenOfOwnerByIndex(msg.sender, i);
+      NftItem storage item = _idToNftItem[tokenId];
+      items[i] = item;
     }
 
     return items;
@@ -134,20 +152,20 @@ contract NftMarket is ERC721URIStorage {
 
     // minting token
     if (from == address(0)) {
-      _addTokenToAllTokensEnumaration(tokenId);
+      _addTokenToAllTokensEnumeration(tokenId);
     }
 
     if (to != from) {
-      _addTokenToOwnerEnumaration(to, tokenId);
+      _addTokenToOwnerEnumeration(to, tokenId);
     }
   }
 
-  function _addTokenToAllTokensEnumaration(uint tokenId) private {
+  function _addTokenToAllTokensEnumeration(uint tokenId) private {
     _idToNftIndex[tokenId] = _allNfts.length;
     _allNfts.push(tokenId);
   }
 
-  function _addTokenToOwnerEnumaration(address to, uint tokenId) private {
+  function _addTokenToOwnerEnumeration(address to, uint tokenId) private {
     uint length = ERC721.balanceOf(to);
     _ownedTokens[to][length] = tokenId;
     _idToOwnedIndex[tokenId] = length;
