641648e975836094118853d2ad1c17f525743c05
Filip Jerga <jerga99@gmail.com>
2022-04-23 15:22:10 +0100

    Get Complete Nfts

--- components/hooks/web3/useListedNfts.ts
+++ components/hooks/web3/useListedNfts.ts
@@ -1,10 +1,11 @@
 
 import { CryptoHookFactory } from "@_types/hooks";
 import { Nft } from "@_types/nft";
+import { ethers } from "ethers";
 import useSWR from "swr";
 
 type UseListedNftsResponse = {}
-type ListedNftsHookFactory = CryptoHookFactory<any, UseListedNftsResponse>
+type ListedNftsHookFactory = CryptoHookFactory<Nft[], UseListedNftsResponse>
 
 export type UseListedNftsHook = ReturnType<ListedNftsHookFactory>
 
@@ -12,9 +13,25 @@ export const hookFactory: ListedNftsHookFactory = ({contract}) => () => {
   const {data, ...swr} = useSWR(
     contract ? "web3/useListedNfts" : null,
     async () => {
+      const nfts = [] as Nft[];
       const coreNfts = await contract!.getAllNftsOnSale();
-      
-      const nfts = [] as any;
+
+      for (let i = 0; i < coreNfts.length; i++) {
+        const item = coreNfts[i];
+        const tokenURI = await contract!.tokenURI(item.tokenId);
+        const metaRes = await fetch(tokenURI);
+        const meta = await metaRes.json();
+
+        nfts.push({
+          price: parseFloat(ethers.utils.formatEther(item.price)),
+          tokenId: item.tokenId.toNumber(),
+          creator: item.creator,
+          isListed: item.isListed,
+          meta
+        })
+      }
+
+      debugger
       return nfts;
     }
   )
