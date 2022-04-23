d317416daad0e4def49e3e419a918430f4ec1af1
Filip Jerga <jerga99@gmail.com>
2022-04-23 15:54:25 +0100

    Active Nft

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -5,6 +5,7 @@ import { BaseLayout } from '@ui'
 
 import { Nft } from '@_types/nft';
 import { useOwnedNfts } from '@hooks/web3';
+import { useEffect, useState } from 'react';
 
 const tabs = [
   { name: 'Your Collection', href: '#', current: true },
@@ -16,6 +17,13 @@ function classNames(...classes: string[]) {
 
 const Profile: NextPage = () => {
   const { nfts }  = useOwnedNfts();
+  const [activeNft, setActiveNft] = useState<Nft>();
+
+  useEffect(() => {
+    if (nfts.data && nfts.data.length > 0) {
+      setActiveNft(nfts.data[0]);
+    }
+  }, [nfts.data])
 
   return (
     <BaseLayout>
@@ -93,26 +101,26 @@ const Profile: NextPage = () => {
 
             {/* Details sidebar */}
             <aside className="hidden w-96 bg-white p-8 border-l border-gray-200 overflow-y-auto lg:block">
-            { false &&
+            { activeNft &&
               <div className="pb-16 space-y-6">
                 <div>
                   <div className="block w-full aspect-w-10 aspect-h-7 rounded-lg overflow-hidden">
-                    <img src={nfts.data[0].meta.image} alt="" className="object-cover" />
+                    <img src={activeNft.meta.image} alt="" className="object-cover" />
                   </div>
                   <div className="mt-4 flex items-start justify-between">
                     <div>
                       <h2 className="text-lg font-medium text-gray-900">
                         <span className="sr-only">Details for </span>
-                        {nfts[0].name}
+                        {activeNft.meta.name}
                       </h2>
-                      <p className="text-sm font-medium text-gray-500">{nfts[0].description}</p>
+                      <p className="text-sm font-medium text-gray-500">{activeNft.meta.description}</p>
                     </div>
                   </div>
                 </div>
                 <div>
                   <h3 className="font-medium text-gray-900">Information</h3>
                   <dl className="mt-2 border-t border-b border-gray-200 divide-y divide-gray-200">
-                    {nfts[0].attributes.map((attr) => (
+                    {activeNft.meta.attributes.map((attr) => (
                       <div key={attr.trait_type} className="py-3 flex justify-between text-sm font-medium">
                         <dt className="text-gray-500">{attr.trait_type}: </dt>
                         <dd className="text-gray-900 text-right">{attr.value}</dd>
