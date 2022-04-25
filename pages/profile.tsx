d02a5f3743ff8ded2f131bad1cde0dae8d780c1d
Filip Jerga <jerga99@gmail.com>
2022-04-25 08:54:04 +0100

    UI changes active nft

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -139,6 +139,7 @@ const Profile: NextPage = () => {
                     Download Image
                   </button>
                   <button
+                    disabled={activeNft.isListed}
                     onClick={() => {
                       nfts.listNft(
                         activeNft.tokenId,
@@ -146,9 +147,9 @@ const Profile: NextPage = () => {
                       )
                     }}
                     type="button"
-                    className="flex-1 ml-3 bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    className="disabled:text-gray-400 disabled:cursor-not-allowed flex-1 ml-3 bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                   >
-                    List Nft
+                    {activeNft.isListed ? "Nft is listed": "List Nft"}
                   </button>
                 </div>
               </div>
