de4dd921d321032e7629c9b2f4708e617a629ab7
Filip Jerga <jerga99@gmail.com>
2022-04-26 11:20:17 +0100

    Get signed data

--- pages/nft/create.tsx
+++ pages/nft/create.tsx
@@ -24,6 +24,19 @@ const NftCreate: NextPage = () => {
     ]
   });
 
+  const getSignedData = async () => {
+    const messageToSign = await axios.get("/api/verify");
+    const accounts = await ethereum?.request({method: "eth_requestAccounts"}) as string[];
+    const account = accounts[0];
+
+    const signedData = await ethereum?.request({
+      method: "personal_sign",
+      params: [JSON.stringify(messageToSign.data), account, messageToSign.data.id]
+    })
+
+    return {signedData, account};
+  }
+
   const handleImage = async (e: ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) {
       console.error("Select a file");
@@ -33,7 +46,13 @@ const NftCreate: NextPage = () => {
     const file = e.target.files[0];
     const buffer = await file.arrayBuffer();
     const bytes = new Uint8Array(buffer);
-    console.log(bytes);
+    
+    try {
+      const {signedData, account} = await getSignedData();
+
+    } catch(e: any) {
+      console.error(e.message);
+    }
   }
 
   const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -54,14 +73,7 @@ const NftCreate: NextPage = () => {
 
   const createNft = async () => {
     try {
-      const messageToSign = await axios.get("/api/verify");
-      const accounts = await ethereum?.request({method: "eth_requestAccounts"}) as string[];
-      const account = accounts[0];
-
-      const signedData = await ethereum?.request({
-        method: "personal_sign",
-        params: [JSON.stringify(messageToSign.data), account, messageToSign.data.id]
-      })
+      const {signedData, account} = await getSignedData();
 
       await axios.post("/api/verify", {
         address: account,
