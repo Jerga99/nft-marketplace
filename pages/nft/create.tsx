a64c0a3c3dfa2633ebd4947158d24d0b52ffd237
Filip Jerga <jerga99@gmail.com>
2022-04-26 12:02:02 +0100

    Fix image select issue

--- pages/nft/create.tsx
+++ pages/nft/create.tsx
@@ -38,7 +38,7 @@ const NftCreate: NextPage = () => {
   }
 
   const handleImage = async (e: ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) {
+    if (!e.target.files || e.target.files.length === 0) {
       console.error("Select a file");
       return;
     }
