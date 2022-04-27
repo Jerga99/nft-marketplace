0a9164838619b7e74fdf1897d75af919f8b3b25a
Filip Jerga <jerga99@gmail.com>
2022-04-27 11:29:29 +0100

    Check network on create page

--- pages/nft/create.tsx
+++ pages/nft/create.tsx
@@ -10,11 +10,14 @@ import axios from 'axios';
 import { useWeb3 } from '@providers/web3';
 import { ethers } from 'ethers';
 import { toast } from "react-toastify";
+import { useNetwork } from '@hooks/web3';
+import { ExclamationIcon } from '@heroicons/react/solid';
 
 const ALLOWED_FIELDS = ["name", "description", "image", "attributes"];
 
 const NftCreate: NextPage = () => {
   const {ethereum, contract} = useWeb3();
+  const {network} = useNetwork();
   const [nftURI, setNftURI] = useState("");
   const [price, setPrice] = useState("");
   const [hasURI, setHasURI] = useState(false);
@@ -150,6 +153,31 @@ const NftCreate: NextPage = () => {
     } catch(e: any) {
       console.error(e.message);
     }
+  }
+
+  if (!network.isConnectedToNetwork) {
+    return (
+      <BaseLayout>
+        <div className="rounded-md bg-yellow-50 p-4 mt-10">
+          <div className="flex">
+            <div className="flex-shrink-0">
+              <ExclamationIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />
+            </div>
+            <div className="ml-3">
+              <h3 className="text-sm font-medium text-yellow-800">Attention needed</h3>
+              <div className="mt-2 text-sm text-yellow-700">
+                <p>
+                { network.isLoading ?
+                  "Loading..." :
+                  `Connect to ${network.targetNetwork}`
+                }
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </BaseLayout>
+    )
   }
 
   return (
