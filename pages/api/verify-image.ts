31e5b17421c8a0f9973d3016e8857b7965b6d336
Filip Jerga <jerga99@gmail.com>
2022-04-27 08:47:42 +0100

    Upload Image success

--- pages/api/verify-image.ts
+++ pages/api/verify-image.ts
@@ -1,7 +1,10 @@
+import { v4 as uuidv4 } from "uuid";
 import { FileReq } from "@_types/nft";
 import { NextApiRequest, NextApiResponse } from "next";
 import { Session } from "next-iron-session";
-import { addressCheckMiddleware, withSession } from "./utils";
+import { addressCheckMiddleware, pinataApiKey, pinataSecretApiKey, withSession } from "./utils";
+import FormData from "form-data";
+import axios from "axios";
 
 export default withSession(async (
   req: NextApiRequest & {session: Session}, 
@@ -20,12 +23,27 @@ export default withSession(async (
 
     await addressCheckMiddleware(req, res);
 
-    console.log(fileName);
-    console.log(contentType);
-    console.log(bytes);
+    const buffer = Buffer.from(Object.values(bytes));
+    const formData = new FormData();
 
-    return res.status(200).send({message: "Image has been created!"});
+    formData.append(
+      "file",
+      buffer, {
+        contentType,
+        filename: fileName + "-" + uuidv4()
+      }
+    );
 
+    const fileRes = await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
+      maxBodyLength: Infinity,
+      headers: {
+        "Content-Type": `multipart/form-data; boundary=${formData.getBoundary()}`,
+        pinata_api_key: pinataApiKey,
+        pinata_secret_api_key: pinataSecretApiKey
+      }
+    });
+
+    return res.status(200).send(fileRes.data);
   } else {
     return res.status(422).send({message: "Invalid endpoint"});
   }
--- pages/nft/create.tsx
+++ pages/nft/create.tsx
@@ -49,13 +49,15 @@ const NftCreate: NextPage = () => {
     
     try {
       const {signedData, account} = await getSignedData();
-      await axios.post("/api/verify-image", {
+      const res = await axios.post("/api/verify-image", {
         address: account,
         signature: signedData,
         bytes,
         contentType: file.type,
         fileName: file.name.replace(/\.[^/.]+$/, "")
       });
+
+      console.log(res.data);
     } catch(e: any) {
       console.error(e.message);
     }
