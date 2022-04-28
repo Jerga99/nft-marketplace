50378f0429d26d354956fe50dfec3056cc260c3b
Filip Jerga <jerga99@gmail.com>
2022-04-28 08:51:07 +0100

    Deploy to Ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const keys = require("./keys.json");
+
 module.exports = {
   contracts_build_directory: "./public/contracts",
   networks: {
@@ -7,6 +10,18 @@ module.exports = {
      port: 7545,
      network_id: "*",
     },
+    ropsten: {
+      provider: () => 
+        new HDWalletProvider(
+          keys.PRIVATE_KEY,
+          keys.INFURA_ROPSTEN_URL
+        ),
+      network_id: 3,
+      gas: 5500000,
+      gasPrice: 20000000000,
+      confirmations: 2,
+      timeoutBlocks: 200
+    }
   },
   compilers: {
     solc: {
